09ef96da8fafa300791a9784d063cd9f104d0061
Dipendu Dey <dipendudey56@gmail.com>
2025-07-14 16:26:02 +0530

    Add files via upload

---
+++ server/routes/event.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getEvents } = require('../controllers/eventController');
+
+
+router.get('/', getEvents);
+
+module.exports = router;
